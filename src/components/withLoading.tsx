949acb9ea11e1ae78b12f941e5674a99ebcbff99
PrisonerM13 <PrisonerM13@outlook.com>
2019-07-28 17:37:40 +0300

    add a comment

--- src/components/withLoading.tsx
+++ src/components/withLoading.tsx
@@ -6,6 +6,10 @@ export interface ILoading {
   onLoad?: () => void;
 }
 
+// HOC which returns a wrapper with two components:
+// 1. The input component with 2 additional props: isLoading and onLoad()
+// 2. Loading indicator component which is displayed when isLoading=true
+// isLoading is initially true. When the consumer calls onLoad() isLoading is set to false.
 const withLoading = (Component: React.FC<ILoading & any> & any) => {
   return ({ ...props }) => {
     const [isLoading, setIsLoading] = useState(true);
